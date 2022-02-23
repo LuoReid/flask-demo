5ed31bdcc57baa20eb922e50b62ec09efa1a41c0
luo.reid <luo.reid@gmail.com>
2022-02-22 19:17:38 -0800

    hello page

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from unicodedata import name
+from flask import Flask,escape, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Flask:)'
+    return 'Welcome, Flasker:)<h1>Hello Totoro:)</h1><img src="http://helloflask.com/totoro.gif">'
 
+@app.route('/home')
+def hello():
+  return 'Welcome to my watchlist.'
+
+@app.route('/user/<name>')
+def user_page(name):
+  return 'Welcome {}:)'.format(escape(name))
+
+@app.route('/test')
+def test_url_for():
+  print(url_for('hello'))
+  print(url_for('user_page',name='Didi'))
+  print(url_for('test_url_for',num=2))
+  return 'Test page'
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=5000)
