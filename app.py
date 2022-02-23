41cd50f284697fadf96db1a0e69f49fcf27657da
luo.reid <luo.reid@gmail.com>
2022-02-22 21:26:13 -0800

    Add template page

--- app.py
+++ app.py
@@ -1,27 +1,39 @@
-from unicodedata import name
-from flask import Flask,escape, url_for
+
+from flask import Flask, escape, url_for, render_template
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'WALL-E', 'year': '2008'},
+]
+
 
 @app.route('/')
-def hello_world():
-    return 'Welcome, Flasker:)<h1>Hello Totoro:)</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
 
 @app.route('/home')
 def hello():
-  return 'Welcome to my watchlist.'
+    return 'Welcome to my watchlist.'
+
 
 @app.route('/user/<name>')
 def user_page(name):
-  return 'Welcome {}:)'.format(escape(name))
+    return 'Welcome {}:)'.format(escape(name))
+
 
 @app.route('/test')
 def test_url_for():
-  print(url_for('hello'))
-  print(url_for('user_page',name='Didi'))
-  print(url_for('test_url_for',num=2))
-  return 'Test page'
+    print(url_for('hello'))
+    print(url_for('user_page', name='Didi'))
+    print(url_for('test_url_for', num=2))
+    return 'Test page'
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
